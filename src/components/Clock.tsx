26fc565a708b4579e2313c1244e4f5c48a43211c
Mr Piyush <taramaelectricalsgroup@gmail.com>
2026-04-07 00:13:52 +0530

    Add useEffect to update time every second

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -16,6 +16,7 @@ const Clock = () => {
   const [ampm, setAmpm] = useState("XX");
   const [date, setDate] = useState(format(new Date(), "eeee, dd LLLL yyyy"));
 
+  
   useEffect(() => {
     const interval = setInterval(() => {
       setHours(format(new Date(), "hh"));
